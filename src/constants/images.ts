67bd3ea6f5dda5fe5b15ae65173045ffbb502624
Stanley Ugwu <stanleyugwu2018@gmail.com>
2025-04-08 12:47:04 +0100

    feat: create images export constant

---
+++ src/constants/images.ts
@@ -0,0 +1,8 @@
+export const Images = {
+  graphPaper: require("../../assets/images/graph_paper.png"),
+  icon: require("../../assets/images/icon.png"),
+  landTopView: require("../../assets/images/land_top_view.png"),
+  onboardingScene1: require("../../assets/images/onboarding_scenes/scene1.png"),
+  onboardingScene2: require("../../assets/images/onboarding_scenes/scene2.png"),
+  onboardingScene3: require("../../assets/images/onboarding_scenes/scene3.png"),
+};
